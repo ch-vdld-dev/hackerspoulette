e3b7beef356990544ea13d4872388cd48a52146b
guide-charon <ch.vdld.dev@gmail.com>
2020-08-31 14:07:53 +0200

    add the logo in the body of the mail

--- assets/php/sendmail.php
+++ assets/php/sendmail.php
@@ -14,18 +14,19 @@
         $mail = new PHPMailer(true);
         try {
             //Server settings
-            $mail->SMTPDebug = 0;                                   // Enable verbose debug output
-            $mail->isSMTP();                                        // Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                   // Set the SMTP server to send through
+            $mail->SMTPDebug = 0;                                        // Enable verbose debug output
+            $mail->isSMTP();                                             // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                        // Set the SMTP server to send through
             $mail->SMTPAuth   = true;
-            $mail->Username   = 'ch.vdld.dev@gmail.com';                     
-            $mail->Password   = '***********';                              
+            $mail->Username   = 'somethin@gmail.com';                     
+            $mail->Password   = 'password here';                              
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         
-            $mail->Port       = 465;                                // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+            $mail->Port       = 465;                                      // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
             //Recipients
             $mail->setFrom('support@hackerspoulette.com', 'SupportTeam');
-            $mail->addAddress($email, $firstName . '' . $lastName);     // Add a recipient
+            $mail->addAddress($email, $firstName . '' . $lastName);       // Add a recipient
+            $mail->AddEmbeddedImage("../img/hackers-poulette-logo.png", "my-logo", "logo.png"); // Add the logo
 
 
             // Content
@@ -33,18 +34,18 @@
             $mail->Subject = 'Team Poulette - Your ' . $subject;
             $mail->Body    = "
                 Dear {$gender} {$lastName} {$firstName},<br />
-                </br>
-                </br>
-                Thanks your sending us your {$subject} !<br />
-                </br>
-                We will give you a feedbac <strong>within 24 hours</strong>. <br />
-                </br>
-                Your message was the following:<br /> <em>{$bodymsg}</em><br />
-                </br>
-                See you soon, <br />
-                </br>
-                </br>
-                <b>Hackers Poulette Team</b><br />
+                <br />
+                <br />
+                This is a confirmation of your {$subject} problem !<br />
+                <br />
+                We will give you a feedback <strong>within 24 hours</strong>. <br />
+                <br />
+                Your message was the following:<br />
+                <br /> <em>{$bodymsg}</em><br />
+                Best regards <br />
+                <br />
+                <b>Support Team / Hackers Poulette Team</b><br />
+                <img alt='PHPMailer' src='cid:my-logo' width='90' height='90'>
                 ";
             $mail->AltBody = 'Hello, Thanks for your feedback. We\ll answer you within 24hours. Hackers Poulettes Team';
 
